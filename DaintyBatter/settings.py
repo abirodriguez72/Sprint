c1c4d121466ba5b5db8fbc78a95e05b73d62a801
shiny-catastrophe <aimee.r.evans@gmail.com>
2025-05-02 16:15:17 -0500

    Updated several pages to correct logout redirect errors

--- DaintyBatter/settings.py
+++ DaintyBatter/settings.py
@@ -62,8 +62,7 @@ ROOT_URLCONF = 'DaintyBatter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Redirect after logout
+LOGOUT_REDIRECT_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
