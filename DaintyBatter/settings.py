23320efa51dd33a921d4be12b796c3b519233cb5
nquiroz10 <neidaquiroz@unomaha.edu>
2025-05-16 14:07:44 -0500

    Update settings.py

--- DaintyBatter/settings.py
+++ DaintyBatter/settings.py
@@ -97,3 +97,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
